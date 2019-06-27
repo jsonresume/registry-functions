f83c7d6a0521e48b8a1a69aaa7ab27547f880690
Thomas Davis <thomasalwyndavis@gmail.com>
2019-06-27 17:47:59 +1000

    Better looking error pages

--- functions/index.js
+++ functions/index.js
@@ -3,52 +3,109 @@ const _ = require("lodash");
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
+const resumeSchema = require("resume-schema");
 const fs = require("fs");
 const app = express();
+// Import Admin SDK
+var admin = require("firebase-admin");
 
+if (process.env.NODE_ENV === "production") {
+  admin.initializeApp(functions.config().firebase);
+} else {
+  var serviceAccount = require("../creds.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://jsonresume-registry.firebaseio.com"
+  });
+}
+
+var db = admin.database();
 app.use(cors({ origin: true }));
+
+const makeTemplate = message => {
+  const template = fs.readFileSync(__dirname + "/template.html", "utf8");
+  return template.replace("{MESSAGE}", message);
+};
 
 app.get("/:username", async (req, res) => {
   const username = req.params.username;
-  if (username === "favicon.ico") {
+  if (
+    [
+      "favicon.ico",
+      "competition",
+      "stats",
+      "apple-touch-icon.png",
+      "apple-touch-icon-precomposed.png",
+      "robots.txt"
+    ].indexOf(username) !== -1
+  ) {
     return res.send(null);
   }
-  console.log(`https://api.github.com/users/${req.params.username}/gists`);
-  const gistData = await axios.get(
-    `https://api.github.com/users/${req.params.username}/gists`
-  );
-  if (!gistData.data) {
-    res.send("This username does not exist on Github");
-  }
-  const resumeUrl = _.find(gistData.data, f => {
-    return f.files["resume.json"];
+  var ref = db.ref();
+  var usersRef = ref.child("gists/" + username);
+  usersRef.on("value", async dataSnapshot => {
+    console.log("=======");
+    console.log(dataSnapshot.val());
+    let gistId;
+    if (!dataSnapshot.val() || !dataSnapshot.val().gistId) {
+      console.log("Fetching gistId");
+      console.log(`https://api.github.com/users/${req.params.username}/gists`);
+      let gistData = {};
+      try {
+        gistData = await axios.get(
+          `https://api.github.com/users/${req.params.username}/gists`
+        );
+      } catch (e) {
+        return res.send(makeTemplate("This is not a valid Github username"));
+      }
+      if (!gistData.data) {
+        return res.send(makeTemplate("This is not a valid Github username"));
+      }
+      const resumeUrl = _.find(gistData.data, f => {
+        return f.files["resume.json"];
+      });
+      if (!resumeUrl) {
+        return res.send(makeTemplate("You have no gists named resume.json"));
+      }
+      gistId = resumeUrl.id;
+    } else {
+      console.log("Using cached gistId");
+      gistId = dataSnapshot.val().gistId;
+    }
+
+    usersRef.set({ gistId: gistId }, () => {});
+    const fullResumeGistUrl =
+      `https://gist.githubusercontent.com/${username}/${gistId}/raw?cachebust=` +
+      new Date().getTime();
+    console.log(fullResumeGistUrl);
+    const resumeRes = await axios({
+      method: "GET",
+      headers: { "content-type": "application/json" },
+      url: fullResumeGistUrl
+    });
+    if (!resumeRes.data) {
+      return res.send(makeTemplate("Something went wrong fetching resume"));
+    }
+    resumeSchema.validate(resumeRes.data, async (err, report) => {
+      console.log("validation finished");
+      if (err) {
+        console.log(err);
+        return res.send(
+          makeTemplate("Resume json invalid - " + JSON.stringify(err))
+        );
+      }
+      const theme =
+        (resumeRes.data.meta && resumeRes.data.meta.theme) || "flat";
+      const resumeHTMLRes = await axios.post(
+        `https://themes.jsonresume.org/theme/${theme}`,
+        { resume: resumeRes.data }
+      );
+      if (!resumeHTMLRes.data) {
+        res.send("There was an error generatoring your resume");
+      }
+      res.send(resumeHTMLRes.data);
+    });
   });
-  if (!resumeUrl) {
-    res.send("You have no gists named resume.json");
-  }
-  const gistId = resumeUrl.id;
-  const options =
-    resumeUrl.description.length > 0 ? JSON.parse(resumeUrl.description) : {};
-  const theme = options.theme || "flat";
-  const fullResumeGistUrl = `https://gist.githubusercontent.com/${username}/${gistId}/raw/`;
-  console.log(fullResumeGistUrl);
-  const resumeRes = await axios({
-    method: "GET",
-    headers: { "content-type": "application/json" },
-    url: fullResumeGistUrl
-  });
-  if (!resumeRes.data) {
-    res.send("Something went wrong fetching resume");
-  }
-  console.log(resumeRes);
-  const resumeHTMLRes = await axios.post(
-    `https://themes.jsonresume.org/theme/${theme}`,
-    { resume: resumeRes.data }
-  );
-  if (!resumeHTMLRes.data) {
-    res.send("There was an error generatoring your resume");
-  }
-  res.send(resumeHTMLRes.data);
 });
 app.listen(3000);
 exports.registry = functions.https.onRequest(app);
