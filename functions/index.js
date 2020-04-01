cdb0e4fc2dd07c2758d90fbbf1b0b6129ac29a73
Thomas Davis <thomasalwyndavis@gmail.com>
2020-04-01 16:28:58 +1100

    testing deploy

--- functions/index.js
+++ functions/index.js
@@ -41,7 +41,7 @@ const getTheme = theme => {
     return {
       e: e.toString(),
       error:
-        "Theme not supported please visit -> https://github.com/jsonresume/registry-functions/issues/7"
+        "Theme is not supported please visit -> https://github.com/jsonresume/registry-functions/issues/7"
     };
   }
 };
