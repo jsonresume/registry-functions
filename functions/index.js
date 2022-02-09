679ea570f61dc13af19d8608e19d186d3f58efec
Ajax Davis <thomasalwyndavis@gmail.com>
2022-02-09 22:50:27 +1100

    less margin on qr

--- functions/index.js
+++ functions/index.js
@@ -155,6 +155,18 @@ app.get("/repo/:username", async (req, res) => {
   });
 });
 
+app.get("/:username.:ext", async (req, res) => {
+  const username = req.params.username;
+  const parsedFormat = req.params.ext.split('.');
+  if (parsedFormat[0] === 'qr' && parsedFormat[1] === 'png') {
+    var code = qr.image('https://registry.jsonresume.org/' + username, { type: 'png', ec_level: 'M', size: 100, margin: 0, parse_url: true });
+    res.setHeader('Content-type', 'image/png');
+    code.pipe(res);
+  } else {
+    res.send('Must be in the format of registry.jsonresume.org/thomasdavis.qr.png')
+  }
+});
+
 app.get("/:username", async (req, res) => {
   const username = req.params.username;
   if (
@@ -257,17 +269,7 @@ app.get("/:username", async (req, res) => {
   });
 });
 
-app.get("/:username.:format", async (req, res) => {
-  const username = req.params.username;
-  const parsedFormat = req.params.format.split('.');
-  if (parsedFormat[0] === 'qr' && parsedFormat[1] === 'png') {
-    var code = qr.image('https://registry.jsonresume.org/' + username, { type: 'png', ec_level: 'M', size: 100, margin: 0, parse_url: true });
-    res.setHeader('Content-type', 'image/png');
-    code.pipe(res);
-  } else {
-    res.send('Must be in the format of registry.jsonresume.org/thomasdavis.qr.png')
-  }
-});
+
 
 
 app.listen(3000);
